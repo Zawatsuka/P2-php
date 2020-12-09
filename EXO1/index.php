86de5791cf545230671ccc7a64c2d01256ed7cf6
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-09 11:46:31 +0100

    premier commit

---
+++ EXO1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXO1</title>
+</head>
+<body>
+    <?php $age= 2 ;
+    if($age <= 18)
+    {
+        echo 'tu es mineur';
+    }
+    else
+    {
+        echo 'tu es majeur';
+    } 
+    ?>
+</body>
+</html>
