f422dfee596a40a8051836683fe64b0b0fe6e974
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-09 12:04:17 +0100

    ajout du 2 et du 3

---
+++ EXO2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo2</title>
+</head>
+<body>
+    <?php $inEasy = true;
+    if($inEasy == true){
+        echo "c'est facile";
+    }
+    else{
+        echo "c'est difficile";
+    }
+?>
+</body>
+</html>
---
+++ EXO3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXO3</title>
+</head>
+<body>
+    <?php $gender = "un homme";
+        $age= "4";
+        if ($age<=18){
+           echo 'Vous etes '.$gender.' et vous etes mineur';
+        }
+        else{
+           echo 'Vous etes '.$gender.' et vous etes majeur';
+        }
+        ?>
+</body>
+</html>
